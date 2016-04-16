df2b5837b4203fc581081d7dc84bd61780a47519
Danny Liu <dannydotliu@gmail.com>
2016-04-16 15:36:19 +1000

    next commit

--- www/js/sres.js
+++ www/js/sres.js
@@ -1,7 +1,7 @@
 
-$.sres.testdata = {};
+$.sres.testonly = {};
 
-$.sres.testdata.testAuthenticationResponse = {
+$.sres.testonly.testAuthenticationResponse = {
 	'result': {
 		'success':true,
 		'message':'welcome'
@@ -15,7 +15,7 @@ $.sres.testdata.testAuthenticationResponse = {
 	},
 	'token':'qoi4nycqi43nyrcqo34y82345nf23597cv36n507387qy4fqny4yvutq'
 };
-$.sres.testdata.testAccessibleColumns = [
+$.sres.testonly.testAccessibleColumns = [
 	{
 		'metadata': {
 			'name':'BIOL123',
@@ -40,7 +40,7 @@ $.sres.testdata.testAccessibleColumns = [
 		]
 	}
 ];
-$.sres.testdata.testColumns = {
+$.sres.testonly.testColumns = {
 	'845ynv8mnc9843mycnm':{
 		'_id':'845ynv8mnc9843mycnm',
 		'name':'week 1 attendance',
@@ -74,6 +74,11 @@ $.sres.testdata.testColumns = {
 	
 	}
 };
+$.sres.testonly.possibleValues = [
+	{'display':'Great', 'value':5},
+	{'display':'OK', 'value':3},
+	{'display':'Poor', 'value':1}
+];
 
 
 /* **********************************
@@ -94,6 +99,53 @@ $.sres.scanControlCodeCallback = function(result) {
 	return true;
 };
 
+/* Saves the current scan entry method */
+$.sres.scanSelectEntryMethod = function() {
+	session.scan.entryMethod = $("input:radio[name=scan_selectentrymethod]:checked").val();
+	$.sres.redirectPage('scan');
+	return true;
+};
+/* Populates scan_dataentry_container */
+$.sres.scanPopulateDataentry = function() {
+	var html = '';
+	var gridBlock = 'b';
+	var valueIndex = 0;
+	
+	// TESTONLY
+	$.sres.testonly.possibleValues.forEach(function(row){
+		if (gridBlock == 'b') { gridBlock = 'a' } else { gridBlock = 'b' };
+		html += '<div class="ui-block-' + gridBlock + '">' + 
+			'<input type="button" ' +
+				'data-valueindex="' + valueIndex + '" ' + 
+				'value="' + row.display + '" ' + 
+				'onclick="javascript:$.sres.dataEntry(\'' + JSON.stringify(row.value) + '\');"' + 
+				'/>' + 
+			'</div>';
+		valueIndex++;
+	});
+	
+	$("#scan_dataentry_buttons").html(html).trigger('create').show();
+	
+	// Show custom input if allowed
+	//if (session.currentColumn.customvalue) {
+		// Custom entry.
+		$("#scan_dataentry_custom_submit").unbind().bind("click", function() {
+			$.sres.dataEntry(JSON.stringify($("#scan_dataentry_custom_input").val()));
+		});
+		// Custom entry pick up enter in input box.
+		$("#scan_dataentry_custom_input").unbind().keyup(function(event) {
+			if (event.which == 13) {
+				event.preventDefault();
+				$("#scan_dataentry_custom_submit").trigger('click');
+			}
+		});
+		// Show container.
+		$("#scan_dataentry_custom").show();
+	//}
+	
+	$("#scan_dataentry_container").show();
+	
+};
 $.sres.scanStudent = function() {
 	$.sres.scanSomething($.sres.scanStudentCallback);
 	return true;
@@ -101,76 +153,57 @@ $.sres.scanStudent = function() {
 $.sres.scanStudentCallback = function(result) {
 	if (result.success && !result.cancelled && result.result) {
 		alert(result.result);
-		// TODO
-		
-		// Identify user // TODO maybe implement this in scan with a callback to here??
-		var users = $.sres.findUsers(result.result);
-		if (users.length > 1) {
-			alert('Warning: more than one user identified');
-		}
-		var user = users[0];
-		
-		// Do different things depending on scanmethod
-		switch (session.currentColumn.scanmethod) {
-			case 'timestamp':
-				// Save data
-				var res = $.sres.saveUserdata(
-					user.id, 
-					session.currentColumn._id, 
-					new Date().toISOString(),
-					$.sres.saveUserdataTimestampCallback
-				);
-				break;
-			case 'scanselect':
-				// Collapse and expand relevant collapsibles
-				$.sres.scanActionsCollapse(true);
-				$.sres.scanDataentryCollapse(false);
-				// Show custom display
-				$.sres.showCustomDisplay(user.id);
-				// Show buttons
-				var html = '';
-				var gridBlock = 'b';
-				var valueIndex = 0;
-				session.currentColumn.possiblevalues.forEach(function(row){
-					if (gridBlock == 'b') { gridBlock = 'a' } else { gridBlock = 'b' };
-					html += '<div class="ui-block-' + gridBlock + '">' + 
-						'<input type="button" ' +
-							'data-valueindex="' + valueIndex + '" ' + 
-							'value="' + row.display + '" ' + 
-							'onclick="javascript:$.sres.saveUserdataScanselect(\'' + user.id + '\',' + valueIndex + ');"' + 
-							'/>' + 
-						'</div>';
-					valueIndex++;
-				});
-				$("#scan_entry_buttons").html(html).trigger('create').show();
-				$("#scan_entry_container").show();
-				// Show input if allowed
-				if (session.currentColumn.customvalue) {
-					$("#scan_entry_custom").show();
+		// Search the server for this user.
+		$.ajax({
+			url: session.api.target + 'api/users/' + encodeURI(result.result),
+			method: 'GET',
+			data: {
+				token: session.api.token
+			},
+			success: function(data) {
+				if (users.length > 1) {
+					alert('Warning: more than one user identified. Using first user found.');
 				}
-				break;
-		}
-		
+				var user = users[0];
+				$.sres.scanUserIdentified(user.id);
+			},
+			error: function(data) {
+				alert('Error finding users');
+			}
+		});
 	} else {
 		alert('Error with scan: ' + JSON.stringify(result));
 	}
 	return true;
 };
-$.sres.scanStudentSelect = function() {
-	
-};
 
-$.sres.identifyPerson = function() {
-	$.sres.redirectPage('identifyperson');
+$.sres.scanUserIdentified = function(userId) {
+	session.scan.targetUserId = userId;
+	switch (session.scan.entryMethod) {
+		case 'selectscan':
+			$.sres.saveUserData(userId);
+			break;
+		case 'scanselect':
+			// Collapse and expand relevant collapsibles.
+			$.sres.scanActionsCollapse(true);
+			$.sres.scanDataentryCollapse(false);
+			// Show custom display.
+			$.sres.showCustomDisplay(userId);
+			// Show data entry.
+			$.sres.scanPopulateDataentry();
+			break;
+	}
+	return true;
 };
 
 $.sres.selectColumn = function() {
+	session.scan = {};
 	$.sres.redirectPage('selectcolumn');
 };
 $.sres.showSelectColumn = function() {
 	$.ajax({
 		url: session.api.target + 'api/papers',
-		type: 'GET',
+		method: 'GET',
 		data: {
 			token: session.api.token
 		},
@@ -178,18 +211,6 @@ $.sres.showSelectColumn = function() {
 			console.log(data);
 			// Show
 			var html = '';
-			// TESTONLY
-			$.sres.testdata.testAccessibleColumns.forEach(function(table){
-				html += '<div data-role="collapsible">';
-				html += '<h3>[TESTONLY] ' + table.metadata.name + '</h3>';
-				html += '<ul data-role="listview">';
-				table.columns.forEach(function(column){
-					html += '<li><a href="javascript:$.sres.activateColumn(\'' + column._id + '\')">' + column.name + '</a></li>';
-				});
-				html += '</ul>';
-				html += '</div>';
-			});
-			
 			data.forEach(function(paper) {
 				html += '<div data-role="collapsible" data-sres-paperid="' + paper.id + '">';
 				html += '<h3>' + paper.code + ' ' + paper.name + '</h3>';
@@ -198,7 +219,7 @@ $.sres.showSelectColumn = function() {
 				html += '</div>';
 				$.ajax({
 					url: session.api.target + 'api/columns/' + paper.id,
-					type: 'GET',
+					method: 'GET',
 					data: {
 						token: session.api.token
 					},
@@ -226,21 +247,44 @@ $.sres.showSelectColumnLoadColumns = function(paperid) {
 	};
 };
 
-$.sres.showIdentifyPerson = function() {
-	
+$.sres.identifyPerson = function() {
+	$.sres.redirectPage('identifyperson');
 };
+$.sres.showIdentifyPerson = function() {
+	$.sres.activateFindPersonFilter('identifyperson_search_results', '$.sres.identifyPersonShowData');
+};
+$.sres.identifyPersonShowData = function(data) {
+	$("a[id=header_rightpanel]").show();
+	alert(data);
+	// TODO
+	
+	$("#rightpanel").panel('open');
+};
+
 
 $.sres.logout = function() {
 	$.sres.clearSession();
 	$("#leftpanel").panel("close");
-	$.sres.redirectPage('login');
+	$.ajax({
+		url: session.api.target + 'api/logout',
+		method: 'GET',
+		data: {
+			token: session.api.token
+		},
+		success: function(data) {
+			$.sres.redirectPage('login');
+		},
+		error: function(data) {
+			alert('Error logging out');
+		}
+	});
 };
 
 $.sres.activateColumn = function(columnid) {
 	// Fetch column info from server
 	$.ajax({
 		url: session.api.target + 'api/column/' + columnid,
-		type: 'GET',
+		method: 'GET',
 		data: {
 			token: session.api.token
 		},
@@ -257,16 +301,44 @@ $.sres.activateColumn = function(columnid) {
 	});
 };
 
+$.sres.activateFindPersonFilter = function(elementId, target) {
+	$("#" + elementId).on('filterablebeforefilter', function(e, data) {
+		var $ul = $(this);
+		var $input = $(data.input);
+		var value = $input.val();
+		var html = "";
+		$ul.html("");
+		if (value && value.length > 2) {
+			$ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
+			$ul.listview('refresh');
+			$.ajax({
+				url: session.api.target + 'api/users/' + encodeURI($input.val()),
+				method: 'GET',
+				data: {
+					token: session.api.token
+				}
+			}).then(function(response) {
+				$.each(response, function(i, data) {
+					html += '<li><a href="javascript:' + target + '(\'' + data.id + '\');">' + data.preferredName + ' ' + data.surname + '</a></li>';
+				});
+				$ul.html(html);
+				$ul.listview('refresh');
+				$ul.trigger('updatelayout');
+			});
+		}
+	});
+};
+
 /* **********************************
 		  Utility functions 
    *********************************** */
 $.sres.loadHeader = function() {
 	if ($.isEmptyObject(session.user)) {
-		$("[data-loggedout]").show();
-		$("[data-loggedin]").hide();
+		$("[data-sres-loggedout]").show();
+		$("[data-sres-loggedin]").hide();
 	} else {
-		$("[data-loggedin]").show();
-		$("[data-loggedout]").hide();
+		$("[data-sres-loggedin]").show();
+		$("[data-sres-loggedout]").hide();
 	}
 };
 
@@ -274,21 +346,28 @@ $.sres.redirectPage = function(pageid, options) {
 	if (typeof options === 'undefined') {
 		options = {'reload':true};
 	}
-	$(":mobile-pagecontainer").pagecontainer("change", $("#" + pageid), options);
+	if ($(":mobile-pagecontainer").pagecontainer("getActivePage").attr("id") == pageid) {
+		// Loading the same page - trigger something.
+		$("#" + pageid).trigger("pagebeforeshow");
+	} else {
+		// Loading a different page - do the usual.
+		$(":mobile-pagecontainer").pagecontainer("change", $("#" + pageid), options);
+	}
 };
 
 $.sres.clearSession = function() {
 	session = {
 		'user':{},
 		'currentColumn':{},
-		'api':{}
+		'api':{},
+		'scan':{}
 	};
 };
 
 $.sres.login = function(username, password, target) {
 	$.ajax({
 		url: target + 'api/login',
-		type: 'POST',
+		method: 'POST',
 		data: {
 			'username':username,
 			'password':password
@@ -350,40 +429,103 @@ $.sres.scanSomething = function(callbackSuccess) {
 			}
 		);
 	} catch (err) {
-		/*alert('Error in scanner: ' + err);
-		return false;*/
-		
-		// TESTONLY
-		res.success = true;
-		res.result = 'somecode';
-		callbackSuccess(res);
-		
+		alert('Error in scanner: ' + err);
+		return false;
 	}
 };
 
-$.sres.findUsers = function(term) {
-	// TODO
+$.sres.saveUserData = function(userId, data) {
+	// Do different things based on entrymethod.
+	switch (session.scan.entryMethod) {
+		case 'selectscan':
+			// There should be data saved already.
+			if (typeof session.scan.selectScanData === 'undefined') {
+				// If not, redirect.
+				alert('No data entered. Redirecting you now...');
+				$.sres.changeEntryMethod();
+				return false;
+			}
+			// Check if data parameter has content, otherwise use previously selected data.
+			if (typeof data === 'undefined') {
+				data = session.scan.selectScanData;
+			}
+			// TODO
+			console.log('selectscan: saving to user these data', userId, data);
+			break;
+		case 'scanselect':
+			// TODO
+			console.log('scanselect: saving to user these data', userId, data);
+			
+			break;
+	}
+	// PUT data to server.
+	$.ajax({
+		url: session.api.target + 'api/userdata',
+		method: 'PUT',
+		data: {
+			token: session.api.token,
+			userid: userId,
+			columnid: session.currentColumn.id,
+			data: data
+		},
+		success: function(data) {
+			console.log(data);
+			alert('hooray');
+		},
+		error: function(data) {
+			alert('Error saving data.');
+			return false;
+		}
+	});
+};
+$.sres.saveUserDataCallback = function() {
 	
-	// TESTONLY
-	var res = [{'id':'u4857394875', 'givennames':'Sammy', 'lastname':'de\'Student', 'email':'sammy.student@uni.edu.xx'}];
-	return res;
 };
 
-$.sres.saveUserdata = function(userid, columnid, data, callbackSuccess) {
+/*	Triggered when some data is entered (either pressing a button or entering custom value).
+	This function needs to decide what to do afterwards.
+*/
+$.sres.dataEntry = function(data) {
+	var value = JSON.parse(data);
+	switch (session.scan.entryMethod) {
+		case 'selectscan':
+			// This means user has just chosen some data to apply to multiple.
+			// Save the selected data/value.
+			session.scan.selectScanData = value;
+			// Show the scan page to scan a student.
+			$.sres.redirectPage('scan');
+			break;
+		case 'scanselect':
+			// This means the user has just chosen some data to apply to an already-scanned student.
+			$.sres.saveUserData(session.scan.targetUserId, value);
+			break;
+	}
+	return true;
+};
+
+$.sres.changeEntryMethod = function() {
+	delete session.scan.entryMethod;
+	delete session.scan.selectScanData;
+	$("#leftpanel").panel('close');
+	$.sres.redirectPage('scan');
+	return true;
+};
+
+/*$.sres.saveUserdata = function(userid, columnid, data, callbackSuccess) {
 	// TODO put call
 	//alert('saving data [' + JSON.stringify(data) + '] for userid [' + userid + '] in columnid [' + columnid + ']');
 	
 	// TESTONLY
 	callbackSuccess(data);
 	return true;
-};
-$.sres.saveUserdataTimestampCallback = function(TESTONLYdata) {
+};*/
+/*$.sres.saveUserdataTimestampCallback = function(TESTONLYdata) {
 	// TESTONLY
 	var res = {'success':true, 'message':'OK', 'userid':'u4857394875', 'currentdata':TESTONLYdata, 'previousdata':'bleh'};
 	$.sres.showSavedData(res);
 	return true;
-};
-$.sres.saveUserdataScanselect = function(userid, index) {
+};*/
+/*$.sres.saveUserdataScanselect = function(userid, index) {
 	$.sres.saveUserdata(
 		userid, 
 		session.currentColumn._id, 
@@ -391,8 +533,8 @@ $.sres.saveUserdataScanselect = function(userid, index) {
 		$.sres.saveUserdataScanselectCallback
 	);
 	return true;
-};
-$.sres.saveUserdataScanselectCustom = function(userid, customdata) {
+};*/
+/*$.sres.saveUserdataScanselectCustom = function(userid, customdata) {
 	$.sres.saveUserdata(
 		userid, 
 		session.currentColumn._id, 
@@ -400,8 +542,8 @@ $.sres.saveUserdataScanselectCustom = function(userid, customdata) {
 		$.sres.saveUserdataScanselectCallback
 	);
 	return true;
-};
-$.sres.saveUserdataScanselectCallback = function(TESTONLYdata) {
+};*/
+/*$.sres.saveUserdataScanselectCallback = function(TESTONLYdata) {
 	// TESTONLY
 	var res = {'success':true, 'message':'OK', 'userid':'u4857394875', 'currentdata':TESTONLYdata, 'previousdata':'bleh'};
 	console.log(res);
@@ -409,7 +551,7 @@ $.sres.saveUserdataScanselectCallback = function(TESTONLYdata) {
 	$.sres.scanActionsCollapse(false);
 	$.sres.scanDataentryCollapse(true);
 	return true;
-};
+};*/
 
 $.sres.showSavedData = function(result) {
 	// Show result
@@ -428,7 +570,6 @@ $.sres.showCustomDisplay = function(userid) {
 	$("#scan_customdisplay").html( $.sres.getCustomDisplay(userid, session.currentColumn._id) ).show();
 	return true;
 };
-
 $.sres.getCustomDisplay = function(userid, columnid) {
 	// TODO
 	
@@ -442,7 +583,7 @@ $.sres.scanActionsCollapse = function(collapse) {
 	return true;
 };
 $.sres.scanDataentryCollapse = function(collapse) {
-	$.sres.changeCollapse('scan_entry_container', collapse);
+	$.sres.changeCollapse('scan_dataentry_container', collapse);
 	return true;
 };
 $.sres.changeCollapse = function(elementId, collapse) {
